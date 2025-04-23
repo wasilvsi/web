ffa0f1d04394958a773240feac17e1b950f9f2ee
wasilvsi <66351998@qq.com>
2025-04-24 00:03:45 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -1,7 +1,7 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
+let userID = '47823477-35ff-47b9-b97d-b73218b8efa7';
 let proxyIP = '';
 //let sub = '';
 let subConverter = atob('U1VCQVBJLkNNTGl1c3Nzcy5uZXQ=');
